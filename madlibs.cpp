cd67bcf284ca6b43976c9e8678942ff3bbf770e6
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-01-31 18:11:37 -0800

    Added code that produces a madlib

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,29 @@
 
 int main()
 {
-
+  int hour;
+  std::cout<< "Name a number 1-24"<<std::endl;
+  std::cin>> hour;
+  int min;
+  std::cout<< "Name a number 1-60"<<std::endl;
+  std::cin>> min;
+  std::string name;
+  std::cout<< "Give a name"<<std::endl;
+  std::cin>> name;
+  std::string place;
+  std::cout<< "Name a place where people meet"<<std::endl;
+  std::cin>> place;
+  std::string name2;
+  std::cout<< "Give another name"<<std::endl;
+  std::cin>> name2;
+  std::string adj;
+  std::cout<< "Give an adjective"<<std::endl;
+  std::cin>> adj;
+  double gpa;
+  std::cout<< "Give a number with decimals bethween 3 and 4"<<std::endl;
+  std::cin>> gpa;
+  std::cout<< "At " << hour << ":" << min << " pm " << name << " went to " << place <<std::endl;
+  std::cout<<" \"man " << name2 << " madlibs are " << adj << " \"" << std::endl;
+  std::cout<<" \"I wish I was a " << gpa << " student\""<<std::endl;
   return 0;
 }
